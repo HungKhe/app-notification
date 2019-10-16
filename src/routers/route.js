99034221072ca7387d0010e45519a23b270bd344
HungKhe <nguyenthanhtung.it79@gmail.com>
2019-10-16 16:51:47 +0700

    Setup app

---
+++ src/routers/route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// router.route('/authenticate')
+//     .get(authToken.initGetCodeToken);
+
+// router.route('/promotion')
+//     .get(promotion.initGetPromotion)
+//     .post(promotion.initAddPromotion)
+
+module.exports = router;
